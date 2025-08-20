e01fbce66f2a16af0ced26d5fc7beb949722df4f
yukenz <yuyun.purniawan@gmail.com>
2025-08-20 23:58:53 +0700

    Spring Init

---
+++ src/main/kotlin/id/co/awan/walle/WalleApplication.kt
@@ -0,0 +1,11 @@
+package id.co.awan.walle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalleApplication
+
+fun main(args: Array<String>) {
+    runApplication<WalleApplication>(*args)
+}
