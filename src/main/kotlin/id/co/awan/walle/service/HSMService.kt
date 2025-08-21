f61d8dc1694a863f265203291d76e4fbc89f1bd4
Yuyun Purniawan <ven.yuyunpurniawan@bankbsi.co.id>
2025-08-21 16:35:28 +0700

    Feat: HSMService

---
+++ src/main/kotlin/id/co/awan/walle/service/HSMService.kt
@@ -0,0 +1,70 @@
+package id.co.awan.walle.service
+
+import id.co.awan.walle.entity.Hsm
+import id.co.awan.walle.repository.HsmRepository
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class HSMService(
+    private val hsmRepository: HsmRepository
+) {
+
+    @Transactional
+    fun changePin(
+        hashCard: String,
+        hashPin: String,
+        newHashPin: String,
+        ownerAddress: String
+    ) {
+        val hsm: Hsm = getHsm(hashCard, hashPin, ownerAddress)
+            ?: throw ResponseStatusException(
+                HttpStatus.NOT_FOUND, "01|HSM not found"
+            )
+        hsm.pin = newHashPin
+
+        hsmRepository.save(hsm)
+    }
+
+
+    fun getHsm(
+        hashCard: String,
+        hashPin: String,
+        ownerAddress: String
+    ): Hsm? {
+        return hsmRepository.findByIdAndPinAndOwnerAddress(
+            hashCard,
+            hashPin,
+            ownerAddress
+        )
+    }
+
+    fun getHsm(
+        hashCard: String,
+        hashPin: String
+    ): Hsm? {
+        return hsmRepository.findByIdAndPin(
+            hashCard,
+            hashPin
+        )
+    }
+
+
+    @Transactional
+    fun resetHsm(
+        hashCardUUID: String
+    ) {
+
+        val hsm = hsmRepository.findById(hashCardUUID)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "01|HSM not found") }!!
+
+        hsm.pin = null
+        hsm.ownerAddress = null
+        hsm.secretKey = null
+
+        hsmRepository.save(hsm)
+    }
+
+}
