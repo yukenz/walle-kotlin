2d005825a30404e7ced68fb6d2be4a582345bf29
yukenz <yuyun.purniawan@gmail.com>
2025-08-24 15:11:01 +0700

    Convert LogUtils to Singleton

--- src/main/kotlin/id/co/awan/walle/utils/LogUtils.kt
+++ src/main/kotlin/id/co/awan/walle/utils/LogUtils.kt
@@ -5,61 +5,59 @@ import org.slf4j.LoggerFactory
 import java.security.SecureRandom
 
 
-class LogUtils {
-    companion object {
+object LogUtils {
 
-        private val log = LoggerFactory.getLogger(this::class.java)
-        private val secureRandom = SecureRandom()
+    private val log = LoggerFactory.getLogger(this::class.java)
+    private val secureRandom = SecureRandom()
 
-        val generateLogToken = {
-            "REQ_${System.currentTimeMillis()}_${secureRandom.nextInt(999999)}"
-        }
-
-        fun logHttpRequest(
-            clazz: Class<*>,
-            methodName: String,
-            request: JsonNode
-        ): String {
-
-            val reqToken = generateLogToken()
-            log.info(
-                "[HTTP-REQUEST {} - {}:{}] : {}",
-                reqToken,
-                clazz.getSimpleName(),
-                "$methodName()",
-                request.toPrettyString()
-            )
-
-            return "$reqToken|$methodName"
-        }
-
-        fun logHttpResponse(
-            reqTokenJoin: String,
-            clazz: Class<*>,
-            response: JsonNode
-        ) {
-            val reqTokenTupple = reqTokenJoin.split("|");
-            log.info(
-                "[HTTP-RESPONSE {} - {}:{}] : {}",
-                reqTokenTupple[0],
-                clazz.getSimpleName(),
-                "${reqTokenTupple[1]}()",
-                response.toPrettyString()
-            )
-        }
-
-        fun logHttpResponseWithoutToken(
-            methodName: String,
-            clazz: Class<*>,
-            response: JsonNode
-        ) {
-            log.info(
-                "[HTTP-RESPONSE - {}:{}] : {}",
-                clazz.getSimpleName(),
-                "$methodName()",
-                response.toPrettyString()
-            )
-        }
-
+    val generateLogToken = {
+        "REQ_${System.currentTimeMillis()}_${secureRandom.nextInt(999999)}"
     }
+
+    fun logHttpRequest(
+        clazz: Class<*>,
+        methodName: String,
+        request: JsonNode
+    ): String {
+
+        val reqToken = generateLogToken()
+        log.info(
+            "[HTTP-REQUEST {} - {}:{}] : {}",
+            reqToken,
+            clazz.getSimpleName(),
+            "$methodName()",
+            request.toPrettyString()
+        )
+
+        return "$reqToken|$methodName"
+    }
+
+    fun logHttpResponse(
+        reqTokenJoin: String,
+        clazz: Class<*>,
+        response: JsonNode
+    ) {
+        val reqTokenTupple = reqTokenJoin.split("|");
+        log.info(
+            "[HTTP-RESPONSE {} - {}:{}] : {}",
+            reqTokenTupple[0],
+            clazz.getSimpleName(),
+            "${reqTokenTupple[1]}()",
+            response.toPrettyString()
+        )
+    }
+
+    fun logHttpResponseWithoutToken(
+        methodName: String,
+        clazz: Class<*>,
+        response: JsonNode
+    ) {
+        log.info(
+            "[HTTP-RESPONSE - {}:{}] : {}",
+            clazz.getSimpleName(),
+            "$methodName()",
+            response.toPrettyString()
+        )
+    }
+
 }
