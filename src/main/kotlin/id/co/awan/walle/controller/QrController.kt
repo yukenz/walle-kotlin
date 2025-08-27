e71451875d883d690c8a3cd96a529048f65e2e13
yukenz <yuyun.purniawan@gmail.com>
2025-08-28 00:48:09 +0700

    Feat: QR Wallet Metadata Inquiry

--- src/main/kotlin/id/co/awan/walle/controller/QrController.kt
+++ src/main/kotlin/id/co/awan/walle/controller/QrController.kt
@@ -2,9 +2,12 @@ package id.co.awan.walle.controller
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import id.co.awan.walle.service.dao.ChainService
+import id.co.awan.walle.service.dao.Erc20MetadataService
+import id.co.awan.walle.service.dao.HSMService
 import id.co.awan.walle.service.dao.MerchantService
-import id.co.awan.walle.service.validation.CardControllerValidation
 import id.co.awan.walle.service.validation.QrControllerValidation
+import id.co.awan.walle.service.web3middleware.ERC20MiddlewareService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -15,8 +18,11 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1/qr")
 class QrController(
     private val merchantService: MerchantService,
-    private val validator: CardControllerValidation,
-    private val qrControllerValidation: QrControllerValidation
+    private val qrControllerValidation: QrControllerValidation,
+    private val hsmService: HSMService,
+    private val chainService: ChainService,
+    private val erc20MetadataService: Erc20MetadataService,
+    private val eRC20MiddlewareService: ERC20MiddlewareService
 ) {
 
     @Operation(summary = "Query QR Merchant")
@@ -39,6 +45,50 @@ class QrController(
         return ResponseEntity.ok(response)
     }
 
+    @Operation(summary = "Query QR Merchant")
+    @GetMapping(
+        path = ["/metadata-inquiry"],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun qrMetadataInquiry(
+        @RequestParam(name = "cardAddress") cardAddress: String,
+        @RequestParam(name = "hashCard") hashCard: String,
+    ): ResponseEntity<*> {
+
+        val walletAddress = hsmService.getOwnerById(hashCard)
+
+        val response = chainService.getAllRegisteredChain()
+            .associate { it1 -> // Loop chain
+                /* return it1 */ try {
+                    val erc20Details = erc20MetadataService.getAllErc20ByChain(it1)
+                        .associate { it2 -> // Loop ERC20 in Chain
+                            /* return it2 */ try {
+                                val allowance = eRC20MiddlewareService.allowance(
+                                    it1.chainName,
+                                    it2.address,
+                                    walletAddress,
+                                    cardAddress,
+                                )
+                                val balanceOf = eRC20MiddlewareService.balanceOf(
+                                    it1.chainName,
+                                    it2.address,
+                                    walletAddress
+                                )
+                                it2.address to arrayOf(allowance, balanceOf)
+                            } catch (e: Exception) {
+                                it2.address to emptyArray()
+                            }
+                        }
+                    it1.chainName to erc20Details
+                } catch (e: Exception) {
+                    it1.chainName to emptyMap()
+                }
+            }
+
+        return ResponseEntity.ok(response)
+    }
+
+
     @Operation(summary = "Payment QR Merchant")
     @PostMapping(
         path = ["/payment"],
@@ -52,13 +102,15 @@ class QrController(
         val (merchantId, cardAddress, amount, chain, erc20Address, hashCard, hashPin, ethSignMessage)
                 = qrControllerValidation.validateQrPayment(request)
 
-        val merchant = merchantService.validateMerchant(merchantId)
+
+// TODO: Diskusi chain Id or Name
+        val findAllERC20Token = chainService.getAllERC20Token(chain)
 
 
-        // TODO: Implement Payment QR
-        // 1. Checking allowance
-        // 2. Sponsor / Recovery Card Gass
-        // 3. TransferFrom
+// TODO: Implement Payment QR
+// 1. Checking allowance
+// 2. Sponsor / Recovery Card Gass
+// 3. TransferFrom
 
         return ResponseEntity.ok().body(null);
 
