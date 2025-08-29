a928466e01e9ecbac5f205e122e8f507f211d95d
Yuyun Purniawan <ven.yuyunpurniawan@bankbsi.co.id>
2025-08-29 14:34:53 +0700

    Working on apart HSM and Wallet

---
+++ src/main/kotlin/id/co/awan/walle/entity/UserWallet.kt
@@ -0,0 +1,16 @@
+package id.co.awan.walle.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "user_wallet", schema = "tap2pay")
+class UserWallet {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    var address: String? = null
+
+}
