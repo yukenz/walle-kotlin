43092ea10d5a1504d5149ef7c42459c6ad5b0895
yukenz <yuyun.purniawan@gmail.com>
2025-08-30 14:10:55 +0700

    Remove unused code

--- src/main/kotlin/id/co/awan/walle/service/common/CryptoGraphyService.kt
+++
@@ -1,8 +0,0 @@
-package id.co.awan.walle.service.common
-
-import id.co.awan.walle.service.core.CryptoGraphyCoreAbstract
-import org.springframework.stereotype.Service
-
-@Service
-class CryptoGraphyService : CryptoGraphyCoreAbstract() {
-}
--- src/main/kotlin/id/co/awan/walle/service/dao/TerminalService.kt
+++ src/main/kotlin/id/co/awan/walle/service/dao/TerminalService.kt
@@ -1,17 +1,11 @@
 package id.co.awan.walle.service.dao
 
-import id.co.awan.walle.entity.Hsm
-import id.co.awan.walle.entity.Merchant
 import id.co.awan.walle.entity.Terminal
 import id.co.awan.walle.repository.HsmRepository
 import id.co.awan.walle.repository.TerminalRepository
-import org.apache.hc.client5.http.utils.Hex
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ResponseStatusException
-import java.security.SecureRandom
-import java.util.*
 
 @Service
 class TerminalService(
